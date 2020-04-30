05665f48ad7cef094fe3484772ab7a6cac0cd742
Rex Bergeron <rexbergeron@comcast.net>
2020-04-30 14:04:01 -0700

    added paging

--- src/components/services/getCharacters.js
+++ src/components/services/getCharacters.js
@@ -1,5 +1,5 @@
-export const fetchCharacters = () => {
-  return fetch('https:rickandmortyapi.com/api/character')
+export const fetchCharacters = (page = 1) => {
+  return fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
     .then(res => res.json())
     .then(json => json.results.map(({ name, description, image }) => ({
       name,
@@ -7,5 +7,4 @@ export const fetchCharacters = () => {
       image
     })));
 };
-
 
